50d1a413094890d25d44e6996c8ec5002f7fb61f
Vincent Joy <vincentjoyhere@gmail.com>
2025-03-13 19:56:30 +0530

    added animation to the entity

--- VoiceARRobot/ViewController.swift
+++ VoiceARRobot/ViewController.swift
@@ -10,10 +10,13 @@ import RealityKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var arView: ARView!
-    private var robotEntity: ModelEntity?
+    
+    private var robotEntity: Entity?
     private var objectAnchor: AnchorEntity?
+    private var moveToLocation: Transform?
+    private var movementDuration: Double = 5 // seconds
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +25,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         startARSession()
         
         // Load 3D Models
-        robotEntity = try! Entity.loadModel(named: "robot")
+        robotEntity = try! Entity.load(named: "robot")
         
         // Tap detector
         arView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:))))
@@ -58,10 +61,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             // Place that 3D model at the plane
             placeObject(object: robotEntity!, at: worldPosition)
+            
+            // Start Moving
+            move(direction: .forward)
         }
     }
     
-    private func placeObject(object modelEntity: ModelEntity, at position: SIMD3<Float>) {
+    private func placeObject(object modelEntity: Entity, at position: SIMD3<Float>) {
         
         // 1. Create anchor (at a 3D position)
         objectAnchor = AnchorEntity(world: position)
@@ -72,4 +78,43 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // 3. Add anchor to scene
         arView.scene.addAnchor(objectAnchor!)
     }
+    
+    private func move(direction: Directions) {
+        switch direction {
+        case .forward:
+            // Move
+            moveToLocation?.translation = robotEntity!.transform.translation + simd_float3(x:0, y:0, z: 20) // Take the robot's current location and move it to 20 centimeters, to move forward
+            robotEntity?.move(to: moveToLocation!, relativeTo: robotEntity, duration: movementDuration)
+            
+            // Animation
+            walkAnimation(movementDuration)
+        case .back:
+            // Move
+            moveToLocation?.translation = robotEntity!.transform.translation + simd_float3(x:0, y:0, z: -20) // Take the robot's current location and move it to -20 centimeters, to move backward
+            robotEntity?.move(to: moveToLocation!, relativeTo: robotEntity, duration: movementDuration)
+            
+            // Animation
+            walkAnimation(movementDuration)
+        case .left:
+            let rotateToAngle = simd_quatf(angle: GLKMathDegreesToRadians(90), axis: SIMD3(x: 0, y: 1, z: 0))
+            robotEntity?.setOrientation(rotateToAngle, relativeTo: robotEntity)
+        case .right:
+            let rotateToAngle = simd_quatf(angle: GLKMathDegreesToRadians(-90), axis: SIMD3(x: 0, y: 1, z: 0))
+            robotEntity?.setOrientation(rotateToAngle, relativeTo: robotEntity)
+        }
+    }
+    
+    private func walkAnimation(_ movementDuration: Double) {
+        // USDZ Animation
+        if let robotAnimation = robotEntity?.availableAnimations.first {
+            // Play the animation
+            robotEntity?.playAnimation(robotAnimation.repeat(duration: movementDuration), transitionDuration: 0.5, startsPaused: false)
+        } else {
+            print("No animation present in the USDZ file")
+        }
+    }
+}
+
+enum Directions: String {
+    case forward, back, left, right
 }
