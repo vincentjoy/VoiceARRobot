9b8aec27259e5072fc6db9fcaadf0b12b1879003
Vincent Joy <vincentjoyhere@gmail.com>
2025-03-13 19:17:35 +0530

    Added the 3D object to the tapped position in the real world

--- VoiceARRobot/ViewController.swift
+++ VoiceARRobot/ViewController.swift
@@ -11,9 +11,65 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
-    @IBOutlet var sceneView: ARView!
+    @IBOutlet var arView: ARView!
+    private var robotEntity: ModelEntity?
+    private var objectAnchor: AnchorEntity?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Start and initialize
+        startARSession()
+        
+        // Load 3D Models
+        robotEntity = try! Entity.loadModel(named: "robot")
+        
+        // Tap detector
+        arView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:))))
+    }
+    
+    private func startARSession() {
+        
+        arView.automaticallyConfigureSession = true
+        
+        // Plane detection
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal]
+        configuration.environmentTexturing = .automatic
+        
+        arView.debugOptions = .showAnchorGeometry
+        arView.session.run(configuration)
+    }
+    
+    @objc
+    private func handleTap(recognizer: UITapGestureRecognizer) {
+        
+        // Touch location
+        let tapLocation = recognizer.location(in: arView)
+        
+        // Raycast (2D -> 3D pos)
+        let results = arView.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .horizontal)
+        
+        // If plane detected
+        if let firstResult = results.first {
+            
+            // 3D pos (x, y, z)
+            let worldPosition = simd_make_float3(firstResult.worldTransform.columns.3)
+            
+            // Place that 3D model at the plane
+            placeObject(object: robotEntity!, at: worldPosition)
+        }
+    }
+    
+    private func placeObject(object modelEntity: ModelEntity, at position: SIMD3<Float>) {
+        
+        // 1. Create anchor (at a 3D position)
+        objectAnchor = AnchorEntity(world: position)
+        
+        // 2. Tie model to anchor
+        objectAnchor!.addChild(modelEntity)
+        
+        // 3. Add anchor to scene
+        arView.scene.addAnchor(objectAnchor!)
     }
 }
