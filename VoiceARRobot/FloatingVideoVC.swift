b0263577e0c81f868c5bba269432dd7c465a7b47
Vincent Joy <vincentjoyhere@gmail.com>
2025-03-17 20:12:55 +0530

    added the video player creation logic

--- VoiceARRobot/FloatingVideoVC.swift
+++ VoiceARRobot/FloatingVideoVC.swift
@@ -1,6 +1,7 @@
 import UIKit
 import RealityKit
 import ARKit
+import AVKit
 
 class FloatingVideoVC: UIViewController, ARSessionDelegate {
     
@@ -30,8 +31,58 @@ class FloatingVideoVC: UIViewController, ARSessionDelegate {
     func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
         for anchor in anchors {
             if let imageAnchor = anchor as? ARImageAnchor {
-                // Place video on image anchor
+                // Create video screen
+                let width = Float(imageAnchor.referenceImage.physicalSize.width)
+                let height = Float(imageAnchor.referenceImage.physicalSize.height)
+                let videoScreen = createVideoScreen(width: width,
+                                                    height: height)
+                
+                // Place that screen on to image anchor
+                
             }
         }
+    }
+    
+    // MARK: - Video Screen
+    
+    func createVideoScreen(width: Float, height: Float) -> ModelEntity {
+        // Mesh
+        let screenMesh = MeshResource.generatePlane(width: width, height: height)
+        
+        // Video material
+        let videoItem = createVideoItem(with: "ElonClip")
+        let videoMaterial = createVideoMaterial(with: videoItem!)
+        
+        // Model entity
+        let videoScreenEntity = ModelEntity(mesh: screenMesh, materials: [videoMaterial])
+        
+        return videoScreenEntity
+    }
+    
+    func createVideoItem(with fileName: String) -> AVPlayerItem? {
+        // URL
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: ".mp4") else {
+            return nil
+        }
+        
+        // Video item
+        let asset = AVURLAsset(url: url)
+        let videoItem = AVPlayerItem(asset: asset)
+        
+        return videoItem
+    }
+    
+    func createVideoMaterial(with videoItem: AVPlayerItem) -> VideoMaterial {
+        // Video player
+        let player = AVPlayer()
+        
+        // Video material
+        let videoMaterial = VideoMaterial(avPlayer: player)
+        
+        // Play video
+        player.replaceCurrentItem(with: videoItem)
+        player.play()
+        
+        return videoMaterial
     }
 }
